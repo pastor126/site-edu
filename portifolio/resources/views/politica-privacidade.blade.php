8f7993cc29b16f7885290881ded31c059d1f40b2
Eduardo C Pacheco <pastor126@gmail.com>
2024-10-24 20:01:01 +0100

    first commit

---
+++ portifolio/resources/views/politica-privacidade.blade.php
@@ -0,0 +1,71 @@
+
+<html>
+    <head><title>Politica de Privacidade</title></head>
+    <meta charset="UTF-8"/>
+    <body>
+     
+<h2>Política de Privacidade para Aplicativos</h2>
+<p>Esta política de privacidade descreve como os aplicativos relacionados abaixo coletam, usam, armazenam e protege as informações dos usuários. Ao utilizar qualquer destes aplicativos, você concorda com os termos desta política.</p>
+
+<ul>
+<li>Capitain's Logbook</li>
+<li>Galeria dos Pastores</li>
+</ul>
+
+<p>Aplicativos de autoria de Eduardo Chagas Pacheco</p>
+
+<p>Informações fornecidas pelo usuário</p>
+
+<p>As informações inseridas pelo usuário, como datas e horários (exceto dados de cadastro de conta caso exista), são armazenadas localmente no dispositivo do usuário e não são compartilhadas com terceiros ou servidores externos. Elas são usadas apenas para o funcionamento correto do aplicativo e para fornecer uma experiência personalizada ao usuário.</p>
+
+<h5>Coleta automática de informações</h5>
+
+<p>Informações como estatísticas de instalação e utilização são coletadas para relatórios estratégicos, sem identificação pessoal.</p>
+
+<h5>Coleta de informações de localização em tempo real</h5>
+
+<p>Este aplicativo não faz a coleta de informações de localização em tempo real.</p>
+
+<h5>Acesso de informações por terceiros</h5>
+
+<p>As informações coletadas estatisticamente podem ser coletadas com terceiros em algumas situações, são elas:</p>
+
+<p>Exigido por lei</p>
+<p>Quando acreditamos de boa fé que a divulgação é necessária para proteger nossos direitos, proteger sua segurança ou a segurança de outras pessoas, investigar fraudes ou responder a uma solicitação do governo.</p>
+</p>Com nossos prestadores de serviços confiáveis que trabalham em nosso nome, não têm um uso independente das informações que divulgamos a eles e concordamos em aderir às regras estabelecidas nesta declaração de privacidade.</p>
+<p>Se eu (Eduardo C Pacheco) estiver envolvido em uma fusão, aquisição ou venda de todos ou de uma parte de seus ativos, você será notificado por e-mail e / ou um aviso destacado em nosso site sobre qualquer alteração na propriedade ou uso dessas informações, bem como quaisquer escolhas que você possa ter em relação a essas informações.</p>
+<p>Para anunciantes e redes de publicidade e empresas de análise de terceiros, conforme descrito na seção abaixo</p>
+
+<h5>Coleta de informações para publicidade</h5>
+
+<p>Trabalhamos com empresas de análise para nos ajudar a entender como o Aplicativo está sendo usado, como a frequência e a duração de uso. Trabalhamos com anunciantes e redes de publicidade de terceiros, que precisam saber como você interage com as publicidades fornecidas ajudando assim manter estes aplicativos gratuitos ou com valores bastante acessíveis.</p>
+
+<p>Informações coletadas pelo Aplicativo para publicidade, incluindo, mas não se limitando, o ID de identificação exclusivo do seu dispositivo móvel e seu número de telefone celular. Para proteger o anonimato dessas informações, usamos uma tecnologia de criptografia para ajudar a garantir que esses terceiros não possam identificá-lo pessoalmente. Esses terceiros também podem obter informações anônimas sobre outros aplicativos que você baixou para seu dispositivo móvel, os sites para celular que você visita, suas informações de localização não precisas (por exemplo, seu CEP) e outras informações de localização não precisas para ajudar a analisar e servir publicidade direcionada anônima no aplicativo e em outros lugares.</p>
+
+<h5>Dos direitos de exclusão</h5>
+
+<p>Desative todas as coletas de informações desinstalando o Aplicativo:  Você pode parar toda a coleta de informações pelo Aplicativo facilmente, desinstalando o Aplicativo. Você pode usar os processos de desinstalação padrão que podem estar disponíveis como parte de seu dispositivo móvel ou via mercado ou rede de aplicativos móveis. </p>
+
+<h5>Segurança</h5>
+
+<p>Estamos preocupados em proteger a confidencialidade de suas informações. Fornecemos proteções físicas, eletrônicas e processuais para proteger as informações que processamos e mantemos. Por favor, esteja ciente de que, embora nos empenhemos em fornecer segurança razoável para as informações que processamos e mantemos, nenhum sistema de segurança pode evitar todas as possíveis violações de segurança.</p>
+
+<h5>Alterações e Consentimento</h5>
+
+<p>Podemos atualizar esta política de privacidade periodicamente. Quando isso acontecer, as alterações serão publicadas nesta página com a data da última atualização. Recomendamos que você reveja esta política regularmente para estar ciente de quaisquer modificações.</p>
+
+<h5>Sobre</h5>
+
+<p>Se você tiver alguma dúvida sobre esta política de privacidade ou sobre as práticas do aplicativo, entre em contato conosco pelo e-mail: pastor126ptg@gmail.com</p>
+
+
+<h5>Eduardo C P</h5>
+
+<address>
+Avenida da Praia, 2311, rc, trás
+Esmoriz - Ovar
+CEP: 3885-405
+</address>
+
+    </body>
+</html>
---
+++ portifolio/resources/views/welcome.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        
+    </head>
+    <body class="antialiased">
+
+      <h1 id="topo" >EduDevTop</h1>
+      <img src="edu.png" id="edu">
+      <p>Sou um desenvolvedor que busca desafios para manter o lema "Do alfinete ao foguete!". Desenvolvo aplicações WEB e <a href="#app">APPs</a> personalizados.</p>
+  <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet odit voluptates nemo earum voluptas. Eius magni nemo quae neque et amet cupiditate, quaerat delectus numquam, autem voluptatibus! Odio, eveniet eius.</div>
+<h2 id="app">Políticas           
+    <p><a href="politica-privacidade">Politica de privacidade para APPs</a></p>
+  
+</h2>
+
+<!-- <ul>
+  <li><a href="https://www.youtube.com/watch?v=Rii25-YJZnY" target="_blank">Brahma, Vishnu e Shiva - A app Hindu</a></li>
+  <li><a href="https://www.youtube.com/watch?v=4UClSFEQRO4" target="_blank">Dattatreya</a></li>
+  <li><a href="https://www.mitografias.com.br/2016/07/a-historia-de-krishna/" target="_blank">Krishna</a></li>
+  <li><a href="https://www.youtube.com/watch?v=2PKQUOXn2PA" target="_blank">Hanuman</a></li>
+</ul>
+    </div> -->
+
+    <!-- <div>
+    <a href="Deuses Hindus.html"><img src="Voltar.jpg" id="voltar"></a>
+    </div> -->
+  </body>
+</html>
+</html>
