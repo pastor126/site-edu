19989b7244c94add25657c84c671a43f548b2e0f
pastor126 <pastor126@gmail.com>
2025-12-19 11:48:29 -0400

    "Corrige sitemap.xml sem view"

--- portifolio/routes/web.php
+++ portifolio/routes/web.php
@@ -46,6 +46,20 @@ Route::get('/sitemap.xml', function () {
        
     ];
     
-    return response()->view('sitemap', compact('urls'))
-        ->header('Content-Type', 'application/xml');
+   $xml = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+    $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+    
+    foreach ($urls as $url) {
+        $xml .= '  <url>' . "\n";
+        $xml .= '    <loc>' . $url['loc'] . '</loc>' . "\n";
+        $xml .= '    <lastmod>' . $url['lastmod'] . '</lastmod>' . "\n";
+        $xml .= '    <changefreq>' . $url['changefreq'] . '</changefreq>' . "\n";
+        $xml .= '    <priority>' . $url['priority'] . '</priority>' . "\n";
+        $xml .= '  </url>' . "\n";
+    }
+    
+    $xml .= '</urlset>';
+    
+    return response($xml, 200)
+        ->header('Content-Type', 'application/xml; charset=UTF-8');
 });
